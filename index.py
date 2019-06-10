4042a716ba256bb94a3d74a82b62425326aeae0e
Manthan1806 <32006356+Manthan1806@users.noreply.github.com>
2019-06-10 23:45:18 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request,flash
+from flask_wtf import Form
+from wtforms import TextField, StringField, validators
+
+app = Flask(__name__)
+app.secret_key = 'development key'
+
+@app.route('/')
+def form():
+   return render_template('form.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+    
