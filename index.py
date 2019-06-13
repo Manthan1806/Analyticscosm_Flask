60dc3d28f1e867ab1fe02121ada8b781de48865c
Manthan1806 <32006356+Manthan1806@users.noreply.github.com>
2019-06-14 01:35:55 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request,flash, send_from_directory
 from flask_wtf import Form
-from wtforms import TextField, StringField, validators
 import os
+from datetime import datetime
+from fpdf import FPDF
+from docx import Document
+from docx.shared import Pt
 
 app = Flask(__name__)
 app.secret_key = 'development key'
@@ -10,28 +13,62 @@ app.secret_key = 'development key'
 def form():
    return render_template('form.html')
 
-@app.route('/result',methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-      result = request.form
-      print(result['Name'])
-      return render_template("result.html",result = result)
-
-@app.route('/download',methods = ['POST', 'GET'])
-def download():
+@app.route('/pdf',methods = ['POST', 'GET'])
+def pdf():
     if request.method == 'POST':
       result = request.form
-      file2 = open("doc.txt","w")
-      print(str(result["Date"]))
-      s1 = "Dear Mr/Ms {} \n\nHope you are well. I have decided to visit you on {} as per you request. This is just a heads-up.\n\nYours truely,\nXXX"
-      print(s1.format(result["Name"],result["Date"]))
-      print(s1)
-      file2.write(s1.format(result["Name"],result["Date"]))
-      file2.close()
+
+      datetimeObject = datetime.strptime(result["Date"],'%Y-%m-%d')
+      newformat = datetimeObject.strftime('%d-%m-%Y')
+      
+      s1 = "Dear Mr/Ms {},"
+      s2 = "Hope you are well. I have decided to visit you on {} as per you request. This is just a heads-up."
+      s3 = "Yours truely,"
+      s4 = "XXX"
+      
+      pdf = FPDF()
+      pdf.add_page()
+      pdf.set_font("Arial", size=12)
+      pdf.cell(150, 10, txt=s1.format(result["Name"]), ln=1, align="L")
+      pdf.cell(0, 10, txt=s2.format(newformat), ln=1, align="C")
+      pdf.cell(190, 10, txt=s3, ln=1, align="R")
+      pdf.cell(190, 10, txt=s4, ln=1, align="R")
+      pdf.output("Letter.pdf")
+
     try:
-        return send_from_directory(os.path.dirname(os.path.abspath(__file__)),'doc.txt',as_attachment=True)
+        return send_from_directory(os.path.dirname(os.path.abspath(__file__)),'Letter.pdf',as_attachment=True)
     except Exception as e:
         return str(e)
+
+@app.route('/docx',methods = ['POST', 'GET'])
+def docx():
+    if request.method == 'POST':
+      result = request.form
+
+      datetimeObject = datetime.strptime(result["Date"],'%Y-%m-%d')
+      newformat = datetimeObject.strftime('%d-%m-%Y')
+
+      document = Document()
+
+      document.add_heading('Letter',0)
+      p1 = document.add_paragraph('Dear Mr/Ms ')
+      p1.add_run(result["Name"]).bold = True
+      p1.add_run(',')
+
+      p2 = document.add_paragraph('Hope you are well. I have decided to visit you on ')
+      p2.add_run(newformat).bold = True
+      p2.add_run(' as per you request. This is just a heads-up.')
+
+      document.add_paragraph('Yours truely,')
+      document.add_paragraph('XXX')
+
+      document.save('Letter.docx')
+
+    try:
+        return send_from_directory(os.path.dirname(os.path.abspath(__file__)),'Letter.docx',as_attachment=True)
+    except Exception as e:
+        return str(e)
+      
 
 if __name__ == '__main__':
    app.run(debug = True)
